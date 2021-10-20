0b1b9ba1f5c4ff73ce106f947d72f3e864b70fb2
zujiang <zuming.jiang@inf.ethz.ch>
2021-10-20 17:14:30 +0200

    update

--- random.cc
+++ random.cc
@@ -5,102 +5,87 @@ namespace smith {
 }
 
 int d6() {
-#ifndef __FILE_RANDOM__
-    static std::uniform_int_distribution<> pick(1, 6);
-    return pick(smith::rng);
-#else
-    static struct file_random_machine* frand = file_random_machine::get(__RAND_FILE_NAME__);
-    return frand->get_random_num(1, 6, 1);
-#endif
+    if (file_random_machine::using_file == NULL) {
+        static std::uniform_int_distribution<> pick(1, 6);
+        return pick(smith::rng);
+    }
+    else
+        return file_random_machine::using_file->get_random_num(1, 6, 1);
 }
 
 int d9() {
-#ifndef __FILE_RANDOM__
-    static std::uniform_int_distribution<> pick(1, 9);
-    return pick(smith::rng);
-#else
-    static struct file_random_machine* frand = file_random_machine::get(__RAND_FILE_NAME__);
-    return frand->get_random_num(1, 9, 1);
-#endif
+    if (file_random_machine::using_file == NULL) {
+        static std::uniform_int_distribution<> pick(1, 9);
+        return pick(smith::rng);
+    }
+    else
+        return file_random_machine::using_file->get_random_num(1, 9, 1);
 }
 
 int d12() {
-#ifndef __FILE_RANDOM__
-    static std::uniform_int_distribution<> pick(1, 12);
-    return pick(smith::rng);
-#else
-    static struct file_random_machine* frand = file_random_machine::get(__RAND_FILE_NAME__);
-    return frand->get_random_num(1, 12, 1);
-#endif
+    if (file_random_machine::using_file == NULL) {
+        static std::uniform_int_distribution<> pick(1, 12);
+        return pick(smith::rng);
+    }
+    else
+        return file_random_machine::using_file->get_random_num(1, 12, 1);
 }
 
 int d20() {
-#ifndef __FILE_RANDOM__
-    static std::uniform_int_distribution<> pick(1, 20);
-    return pick(smith::rng);
-#else
-    static struct file_random_machine* frand = file_random_machine::get(__RAND_FILE_NAME__);
-    return frand->get_random_num(1, 20, 1);
-#endif
+    if (file_random_machine::using_file == NULL) {
+        static std::uniform_int_distribution<> pick(1, 20);
+        return pick(smith::rng);
+    }
+    else
+        return file_random_machine::using_file->get_random_num(1, 20, 1);
 }
 
 int d42() {
-#ifndef __FILE_RANDOM__
-    static std::uniform_int_distribution<> pick(1, 42);
-    return pick(smith::rng);
-#else
-    static struct file_random_machine* frand = file_random_machine::get(__RAND_FILE_NAME__);
-    return frand->get_random_num(1, 42, 2);
-#endif
+    if (file_random_machine::using_file == NULL) {
+        static std::uniform_int_distribution<> pick(1, 42);
+        return pick(smith::rng);
+    }
+    else
+        return file_random_machine::using_file->get_random_num(1, 42, 2);
 }
 
 int d100() {
-#ifndef __FILE_RANDOM__
-    static std::uniform_int_distribution<> pick(1, 100);
-    return pick(smith::rng);
-#else
-    static struct file_random_machine* frand = file_random_machine::get(__RAND_FILE_NAME__);
-    return frand->get_random_num(1, 100, 2);
-#endif
+    if (file_random_machine::using_file == NULL) {
+        static std::uniform_int_distribution<> pick(1, 100);
+        return pick(smith::rng);
+    }
+    else 
+        return file_random_machine::using_file->get_random_num(1, 100, 2);
 }
 
 // 1 - x
 int dx(int x) {
-#ifndef __FILE_RANDOM__
-    std::uniform_int_distribution<> pick(1, x);
-    return pick(smith::rng);
-#else
-    static struct file_random_machine* frand = file_random_machine::get(__RAND_FILE_NAME__);
-    if (x == 1)
-        return 1;
-    
-    int bytenum;
-    if (x <= 0xff >> 3) 
-        bytenum = 1;
-    else if (x <= 0xffff >> 3)
-        bytenum = 2;
-    else if (x <= 0xffffff >> 3)
-        bytenum = 3;
-    else
-        bytenum = 4;
-    return frand->get_random_num(1, x, bytenum);
-#endif
+    if (file_random_machine::using_file == NULL) {
+        std::uniform_int_distribution<> pick(1, x);
+        return pick(smith::rng);
+    }
+    else {
+        if (x == 1)
+            return 1;
+        int bytenum;
+        if (x <= (0xff >> 3)) 
+            bytenum = 1;
+        else if (x <= (0xffff >> 3))
+            bytenum = 2;
+        else if (x <= (0xffffff >> 3))
+            bytenum = 3;
+        else
+            bytenum = 4;
+        return file_random_machine::using_file->get_random_num(1, x, bytenum);
+    }
 }
 
 std::string random_identifier_generate() {
-    unsigned static int seed = 1;
-    unsigned int rand_seed = seed * 131;
-    unsigned int rand_value = 0;
-    while (rand_seed != 0) {
-        rand_value = rand_value * 131;
-        rand_value = rand_value + (rand_seed & 0x1);
-        rand_seed = rand_seed >> 1;
-    }
-    
+    unsigned int rand_value = (dx(0xffffff) << 8) + dx(0xff); // 5 bytes
     std::string name;
     while (rand_value != 0) {
-        unsigned int choice = (rand_value & 0xff) % 37 + 1;
-        rand_value = rand_value >> 8;
+        unsigned int choice = (rand_value & 0x3f) % 37 + 1;
+        rand_value = rand_value >> 6;
 
         // 1-37
         if (choice <= 26) // 1 - 26
@@ -110,9 +95,7 @@ std::string random_identifier_generate() {
         else if (choice <= 37) // 28 - 37
             name.push_back('0' + choice - 28);
     }
-    seed++;
     return name;
-
 }
 
 file_random_machine::file_random_machine(string s)
@@ -144,13 +127,10 @@ file_random_machine::~file_random_machine()
 {
     if (buffer != NULL)
         delete[] buffer;
-    
-    std::ofstream fout(__READ_BYTE_NUM__);
-    fout << read_byte << endl;
-    fout.close();
 }
 
 map<string, struct file_random_machine*> file_random_machine::stream_map;
+struct file_random_machine* file_random_machine::using_file = NULL;
 
 struct file_random_machine *file_random_machine::get(string filename)
 {
@@ -158,6 +138,16 @@ struct file_random_machine *file_random_machine::get(string filename)
         return stream_map[filename];
     else
         return stream_map[filename] = new file_random_machine(filename);
+}
+
+bool file_random_machine::map_empty()
+{
+    return stream_map.empty();
+}
+
+void file_random_machine::use_file(string filename)
+{
+    using_file = get(filename);
 }
 
 int file_random_machine::get_random_num(int min, int max, int byte_num)
--- random.hh
+++ random.hh
@@ -5,8 +5,8 @@
 #define RANDOM_HH
 
 // #define __FILE_RANDOM__
-#define __RAND_FILE_NAME__ "rand_file.txt"
-#define __READ_BYTE_NUM__ "read_byte.txt"
+// #define __RAND_FILE_NAME__ "rand_file.txt"
+// #define __READ_BYTE_NUM__ "read_byte.txt"
 
 #include <random>
 #include <utility>
@@ -42,8 +42,13 @@ struct file_random_machine {
     int cur_pos;
     int end_pos;
     int read_byte;
+
+    static struct file_random_machine *using_file;
     static map<string, struct file_random_machine*> stream_map;
     static struct file_random_machine *get(string filename);
+    static bool map_empty();
+    static void use_file(string filename);
+    
     file_random_machine(string s);
     ~file_random_machine();
     int get_random_num(int min, int max, int byte_num);
@@ -53,12 +58,12 @@ template<typename T> T& random_pick(std::vector<T>& container) {
     if (!container.size())
         throw std::runtime_error("No candidates available");
 
-#ifndef __FILE_RANDOM__
-    std::uniform_int_distribution<int> pick(0, container.size()-1);
-    return container[pick(smith::rng)];
-#else
-    return container[dx(container.size()) - 1];
-#endif
+    if (file_random_machine::using_file == NULL) {
+        std::uniform_int_distribution<int> pick(0, container.size()-1);
+        return container[pick(smith::rng)];
+    }
+    else
+        return container[dx(container.size()) - 1];
 }
 
 template<typename I>
@@ -66,14 +71,16 @@ I random_pick(I beg, I end) {
     if (beg == end)
       throw std::runtime_error("No candidates available");
 
-#ifndef __FILE_RANDOM__
-    std::uniform_int_distribution<> pick(0, std::distance(beg, end) - 1);
-    std::advance(beg, pick(smith::rng));
-    return beg;
-#else
-    std::advance(beg, dx(std::distance(beg, end)) - 1);
-    return beg;
-#endif
+    if (file_random_machine::using_file == NULL) {
+        std::uniform_int_distribution<> pick(0, std::distance(beg, end) - 1);
+        std::advance(beg, pick(smith::rng));
+        return beg;
+    }
+    else {
+        std::cerr << "p = " << file_random_machine::using_file << endl;
+        std::advance(beg, dx(std::distance(beg, end)) - 1);
+        return beg;
+    }
 }
 
 template<typename I>
