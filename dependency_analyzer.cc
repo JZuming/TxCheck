c90c0c28b4ebef1dfccba63502ff033146d67718
zujiang <zuming.jiang@inf.ethz.ch>
2022-03-30 23:12:22 +0200

    partially add dependency analysis

--- dependency_analyzer.cc
+++ dependency_analyzer.cc
@@ -1,14 +1,90 @@
 #include <dependency_analyzer.hh>
 
-dependency_analyzer::dependency_analyzer(vector<one_output>& total_output,
+void history::insert_to_history(operate_unit& oper_unit)
+{
+    auto row_id = oper_unit.row_id;
+    auto size = change_history.size();
+    bool exist_row_id = false;
+    int row_idx;
+    for (int i = 0; i < size; i ++) {
+        if (change_history[i].row_id == row_id) {
+            exist_row_id = true;
+            row_idx = i;
+            break;
+        }
+    }
+
+    if (exist_row_id) 
+        change_history[row_idx].row_op_list.push_back(oper_unit);
+    else {
+        row_change_history rch;
+        rch.row_id = row_id;
+        rch.row_op_list.push_back(oper_unit);
+        change_history.push_back(rch);
+    }
+
+    return;
+}
+
+size_t dependency_analyzer::hash_output(row_output& row)
+{
+    register size_t hash = 0; 
+    for (auto& str:row) {
+        auto size = str.size();
+        for (int i = 0; i < size; i++ ) {
+            size_t ch = (size_t)str[i];
+            hash = hash * 131 + ch;
+        }
+    }
+    return hash;
+}
+
+void dependency_analyzer::build_WR_dependency(vector<operate_unit>& op_list, int op_idx)
+{
+    auto& target_op = op_list[op_idx];
+    bool find_the_write = false;
+    for (int i = op_idx - 1; i >= 0; i--) {
+        if (op_list[i].stmt_u != AFTER_WRITE_READ)
+            continue;
+        if (op_list[i].hash != target_op.hash)
+            continue;
+        
+        find_the_write = true;
+
+        if (op_list[i].tid == target_op.tid)
+            continue;
+
+        dependency_graph[op_list[i].tid][target_op.tid].push_back(WRITE_READ);
+    }
+    if (find_the_write == false)
+        throw runtime_error("BUG: Cannot find the corresponding write");
+    return;
+}
+
+dependency_analyzer::dependency_analyzer(vector<one_output>& init_output,
+                        vector<one_output>& total_output,
                         vector<int>& final_tid_queue,
                         vector<stmt_usage>& final_stmt_usage,
-                        int t_num):
+                        int t_num,
+                        int primary_key_idx,
+                        int write_op_key_idx):
 tid_num(t_num)
 {   
-    dependency_graph = new dependency_type* [tid_num];
+    dependency_graph = new vector<dependency_type>* [tid_num];
     for (int i = 0; i < tid_num; i++) 
-        dependency_graph[i] = new dependency_type [tid_num];
+        dependency_graph[i] = new vector<dependency_type> [tid_num];
+
+    for (auto& each_output : init_output) {
+        if (each_output.empty())
+            continue;
+        for (auto& row : each_output) {
+            auto row_id = stoi(row[primary_key_idx]);
+            auto write_op_id = stoi(row[write_op_key_idx]);
+            auto hash = hash_output(row);
+            operate_unit op(AFTER_WRITE_READ, write_op_id, -1, -1, row_id, hash);
+            h.insert_to_history(op);
+        }
+    }
     
     auto stmt_num = final_tid_queue.size();
     for (int i = 0; i < stmt_num; i++) {
@@ -21,9 +97,29 @@ tid_num(t_num)
         if (each_output.empty())
             continue;
         
-        if (stmt_u == NORMAL) { // analyze WR dependency and store the read result
-            
+        for (auto& row : each_output) {
+            auto row_id = stoi(row[primary_key_idx]);
+            auto write_op_id = stoi(row[write_op_key_idx]);
+            auto hash = hash_output(row);
+            operate_unit op(stmt_u, write_op_id, tid, i, row_id, hash);
+            h.insert_to_history(op);
         }
+    }
+
+    for (auto& row_history : h.change_history) {
+        auto& row_op_list = row_history.row_op_list;
+        auto size = row_op_list.size();
+        for (int i = 0; i < size; i++) {
+            auto& op_unit = row_op_list[i];
+            if (op_unit.stmt_u == NORMAL) 
+                build_WR_dependency(row_op_list, i);
+            if (op_unit.stmt_u == BEFORE_WRITE_READ) {
+                build_WR_dependency(row_op_list, i); // it is a read itself
+                
+            }
+        }
+
+
     }
 }
 
--- dependency_analyzer.hh
+++ dependency_analyzer.hh
@@ -16,18 +16,49 @@ using namespace std;
 
 enum dependency_type {WRITE_READ, WRITE_WRITE, READ_WRITE};
 
-typedef vector<string> row; // a row consists of several field(string)
-typedef vector<row> one_output; // one output consits of several rows
+typedef vector<string> row_output; // a row consists of several field(string)
+typedef vector<row_output> one_output; // one output consits of several rows
+
+struct operate_unit {
+    stmt_usage stmt_u;
+    int write_op_id;
+    int tid;
+    int stmt_idx;
+    int row_id;
+    size_t hash;
+    operate_unit(stmt_usage use, int op_id, int tid, int stmt_idx, int row_id, size_t hash):
+        stmt_u(use), write_op_id(op_id), 
+        tid(tid), stmt_idx(stmt_idx), 
+        row_id(row_id), hash(hash) {}
+};
+
+struct row_change_history {
+    int row_id;
+    vector<operate_unit> row_op_list;
+};
+
+struct history {
+    vector<row_change_history> change_history;
+    void insert_to_history(operate_unit& oper_unit);
+};
 
 struct dependency_analyzer
 {
-    dependency_analyzer(vector<one_output>& total_output,
+    dependency_analyzer(vector<one_output>& init_output,
+                        vector<one_output>& total_output,
                         vector<int>& final_tid_queue,
                         vector<stmt_usage>& final_stmt_usage,
-                        int t_num);
+                        int t_num,
+                        int primary_key_idx,
+                        int write_op_key_idx);
     ~dependency_analyzer();
+
+    size_t hash_output(row_output& row);
+    void build_WR_dependency(vector<operate_unit>& op_list, int op_idx);
+
+    history h;
     int tid_num;
-    dependency_type **dependency_graph;
+    vector<dependency_type> **dependency_graph;
 };
 
 #endif
